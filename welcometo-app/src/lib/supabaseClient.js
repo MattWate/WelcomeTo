170985771bb2087e22b3ab6582d17275d60697ef
MattWate <matthew@dubyahinnovation.com>
2025-08-25 16:49:06 +0200

    Update supabaseClient.js

--- welcometo-app/src/lib/supabaseClient.js
+++ welcometo-app/src/lib/supabaseClient.js
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Your project's specific Supabase URL and public anon key.
-const supabaseUrl = 'https://wopdpporlylygxyvpene.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvcGRwcG9ybHlseWd4eXZwZW5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwOTg0MjIsImV4cCI6MjA2ODY3NDQyMn0.ub5LP_93NcC6wkJbkQWkJ6oBLTKrUNJIiZJosgA9c6Q';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Create and export the Supabase client.
-// This single 'supabase' object will be used throughout your app 
-// to interact with your database and authentication.
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default supabase;
